7c92453f249b874f973de6fd75be24ebcf813714
Augusto Gonzalez Bonorino <agonz439@asu.edu>
2026-04-22 13:12:26 -0700

    Import optimization preamble domain

--- lean_workspace/LeanEcon/Preamble.lean
+++ lean_workspace/LeanEcon/Preamble.lean
@@ -3,4 +3,5 @@ import LeanEcon.Preamble.Microeconomics
 import LeanEcon.Preamble.Macroeconomics
 import LeanEcon.Preamble.GameTheory
 import LeanEcon.Preamble.GeneralEquilibrium
+import LeanEcon.Preamble.Optimization
 import LeanEcon.Preamble.Tools
